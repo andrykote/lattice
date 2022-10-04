29b430c71f12692f1ccf166e4819b60906d2d6a9
Andre Gil <andrykote@gmail.com>
2022-10-05 00:41:29 +0300

    create dirty header and top main

---
+++ sript.js
@@ -0,0 +1,8 @@
+let burger = document.querySelector('.burger');
+// let list = document.querySelector('.header-nav')
+
+burger.addEventListener('click', () => {
+	// list.classList.toggle('open')
+	burger.classList.toggle('open')
+})
+
