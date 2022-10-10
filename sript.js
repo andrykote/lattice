bb176d80dac9466c1fa8e7083461a056d4268a40
Andre Gil <andrykote@gmail.com>
2022-10-10 20:52:45 +0300

    add listner on select

--- sript.js
+++ sript.js
@@ -1,7 +1,41 @@
-let burger = document.querySelector('.burger');
-// let list = document.querySelector('.header-nav')
+// let burger = document.querySelector(".burger");
+// let selection = document.querySelector(".selection-header");
+// let selectionList = document.querySelector(".item-box");
+// let selectionBox = document.querySelector(".lattice-size-box");
 
-burger.addEventListener('click', () => {
-	// list.classList.toggle('open')
-	burger.classList.toggle('open')
+// // Header burger open/closed
+// burger.addEventListener("click", () => {
+//   // list.classList.toggle('open')
+//   burger.classList.toggle("open");
+// });
+
+// selection.addEventListener("click", openCloseModal);
+
+// selectionList.addEventListener("click", setValue);
+
+// function openCloseModal() {
+//   selectionList.classList.toggle("item-box--open");
+// }
+
+// function setValue(e) {
+//   if (e.target.classList.contains("selection-item")) {
+//     let selectionValue = document.querySelector(".selection-value");
+//     selectionValue.textContent = e.target.getAttribute("value");
+//     selectionList.classList.toggle("item-box--open");
+//   } else return;
+// }
+
+let box = document.querySelector('.lattice-size-box')
+console.log(box);
+
+box.addEventListener('click', ({target}) => {
+	if(target.closest(".selection")) {
+		let selection = target.closest(".selection");
+		let itemBox = selection.querySelector('.item-box')
+
+		itemBox.classList.toggle("item-box--open")
+	}
+
+	
 })
+
